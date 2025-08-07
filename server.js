2e11d443f81e551a13839b0fa2585e238b25e11e
asitmandal <asitmandal492@gmail.com>
2025-08-07 14:22:37 +0530

    this

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.locals = {
 // app.use(cors());
 
 const corsOptions = {
-  origin: 'https://ui.torisedigital.com', // ya koi aur specific domain
+  origin: '*', // ya koi aur specific domain
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // agar cookies ya auth headers bhejne ho
